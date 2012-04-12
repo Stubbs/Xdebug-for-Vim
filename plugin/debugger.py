af107b9dd1035358fe1603e9a028f6024fe0f917
Stuart Grimshaw <stuart@localphone.com>
2012-04-12 08:44:04 +0100

    Added dbgTimeout config param to set debug timeout.

--- plugin/debugger.py
+++ plugin/debugger.py
@@ -525,8 +525,8 @@ class DebugUI:
     import re
     map_sep = re.compile(',\s*')
     path_sep = re.compile('\s*:\s*')
-    if vim.eval('exists("pathMap")') != "0":
-        mappings = map_sep.split(vim.eval('pathMap'))
+    if vim.eval('exists("g:pathMap")') != "0":
+        mappings = map_sep.split(vim.eval('g:pathMap'))
         if mappings:
           for mapping in mappings:
              (remote_path, local_path) = path_sep.split(mapping)
@@ -561,7 +561,7 @@ class DbgProtocol:
   def isconnected(self):
     return self.isconned
   def accept(self):
-    print 'waiting for a new connection on port '+str(self.port)+' for 5 seconds...'
+    print 'waiting for a new connection on port '+str(self.port)+' for '+str( socket.getdefaulttimeout() )+' seconds...'
     serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
       serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -674,9 +674,10 @@ class Debugger:
   #################################################################################################################
   # Internal functions
   #
-  def __init__(self, port = 9000, max_children = '32', max_data = '1024', max_depth = '1', minibufexpl = '0', debug = 0):
+  def __init__(self, port = 9000, max_children = '32', max_data = '1024', max_depth = '1', minibufexpl = '0', debug = 0, timeout = 5):
     """ initialize Debugger """
-    socket.setdefaulttimeout(5)
+    socket.setdefaulttimeout(timeout)
+    self.timeout    = timeout
     self.port       = port
     self.debug      = debug
 
@@ -1000,8 +1001,8 @@ class Debugger:
       map_sep = re.compile(',\s*')
       path_sep = re.compile('\s*:\s*')
       remote_file = file
-      if vim.eval('exists("pathMap")') != "0":
-          mappings = map_sep.split(vim.eval('pathMap'))
+      if vim.eval('exists("g:pathMap")') != "0":
+          mappings = map_sep.split(vim.eval('g:pathMap'))
           if mappings:
             for mapping in mappings:
                (remote_path, local_path) = path_sep.split(mapping)
@@ -1067,6 +1068,10 @@ def debugger_init(debug = 0):
   if port == 0:
     port = 9000
 
+  timeout = int(vim.eval('g:dbgTimeout'))
+  if timeout == 0:
+      timeout = 5
+
   # the max_depth variable to set in the engine
   max_children = vim.eval('debuggerMaxChildren')
   if max_children == '':
@@ -1084,7 +1089,7 @@ def debugger_init(debug = 0):
   if minibufexpl == 0:
     minibufexpl = 0
 
-  debugger  = Debugger(port, max_children, max_data, max_depth, minibufexpl, debug)
+  debugger  = Debugger(port, max_children, max_data, max_depth, minibufexpl, debug, timeout)
 
 def debugger_command(msg, arg1 = '', arg2 = ''):
   try:
